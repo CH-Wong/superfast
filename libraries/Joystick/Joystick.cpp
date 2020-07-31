6a682558c550d486079fcc0a305104eb7ceb1c71
Kiefer Vermeulen <kiefervermeulen@hotmail.com>
2020-07-31 15:51:44 +0200

    Added Joystick library

---
+++ libraries/Joystick/Joystick.cpp
@@ -0,0 +1,40 @@
+/*
+  Joystick.cpp - Library for control using joystick.
+  Created by Ya boi.
+  Released into the public domain.
+*/
+
+#include "Arduino.h"
+#include "Joystick.h"
+
+Joystick::Joystick(int pinX, int pinY, int pinButton)
+{ 
+   _pinX = pinX;
+   _pinY = pinY;
+   _pinButton = pinButton;
+   int value = 0;
+   int x_value = 0;
+   int y_value = 0;
+   int x_out = 0; 
+   int button_value = 0;
+   int buffer = 10;
+//pinMode(_pinButton, INPUT_PULLUP); This is for HONKYBOI
+}
+
+int Joystick::angle()
+{
+   x_value = analogRead(_pinX);
+   if (x_value >= (1024/2-buffer) && x_value <=1024/2+buffer)
+      {
+         x_out = 512;
+      }
+   else{ x_out = x_value ;}
+      return map(X_out,0,1023,0,180);
+   delay(100);
+}
+
+
+
+
+    
+  
