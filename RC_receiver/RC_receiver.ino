b0214cfe9251c47ed99651f7d9134448ef254345
CH-Wong <w.chunheung@gmail.com>
2020-08-12 16:25:10 +0200

    receiver code

--- RC_receiver/RC_receiver.ino
+++ RC_receiver/RC_receiver.ino
@@ -10,10 +10,15 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-// Default pins:
-// SCK: 13
-// MISO: 12
-// MOSI: 11
+#include <Servo.h>
+
+
+/* Set up RF Receiver
+* Default pins:
+* SCK: 13
+* MISO: 12
+* MOSI: 11
+*/
  
 // Define control pins
 RF24 radio(9, 10); // CE, CSN
@@ -21,17 +26,66 @@ RF24 radio(9, 10); // CE, CSN
 // Select Channel
 const byte address[6] = "00001";
 
+// Set up Servomotor
+Servo steeringServo;  // create servo object to control a servo
+int pinServo = 7;
+int input_pos = 90;    // variable to store the servo position, start straight (90deg)
+int curr_pos = 90;
+int pos = 0;
+
+
 void setup() {
   Serial.begin(9600);
+
+  // Initialize servo motor
+  steeringServo.write(curr_pos);
+  // Set up Joystick read-out.
+  steeringServo.attach(pinServo);  // attaches the servo on pin 9 to the servo object
+
+   for (curr_pos = -1; curr_pos <= 90; curr_pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    steeringServo.write(curr_pos);              // tell servo to go to position in variable 'pos'
+    delay(10);                       // waits 15ms for the servo to reach the position
+  }
+  
+  for (curr_pos = 181; curr_pos >= 90; curr_pos -= 1) { // goes from 180 degrees to 0 degrees
+    steeringServo.write(curr_pos);              // tell servo to go to position in variable 'pos'
+    delay(10);                       // waits 15ms for the servo to reach the position
+  }
+
+  // Intitialize RF transceiving  
   radio.begin();
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
 }
+
 void loop() {
   if (radio.available()) {
     char text[32] = "";
     radio.read(&text, sizeof(text));
     Serial.println(text);
+    
+    input_pos = text.toInt();
+    
+    if (input_pos > curr_pos && curr_pos < 180){
+      curr_pos += 1;
+    }
+  
+    else if (input_pos < curr_pos && curr_pos > 0){
+      curr_pos -= 1;
+    }
+  
+    steeringServo.write(curr_pos);
+    delay(10);
+  
   }
+
+  
+  else{
+    Serial.println("off");
+  }
+
+
+  
 }
