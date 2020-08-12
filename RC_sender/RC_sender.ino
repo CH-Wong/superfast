aad63672628ae3a2afa397c915133f636789bc9f
Kiefer Vermeulen <kiefervermeulen@hotmail.com>
2020-08-12 17:05:34 +0200

    Changed address of send back to 00001

--- RC_sender/RC_sender.ino
+++ RC_sender/RC_sender.ino
@@ -2,37 +2,36 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <Joystick.h>
+
 RF24 radio(9, 10); // CE, CSN     
+const byte address[6] = "00001";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
+
 int pinX = A0;
 int pinY = A1;
 int pinButton = 7;
-Joystick joystick(pinX, pinY, pinButton);    
-const byte address[6] = "00001";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
-
 int input_pos = 90;
 boolean button_state = 0;
+
+Joystick joystick(pinX, pinY, pinButton);    
+
+
 void setup() {
   Serial.begin(9600);
-radio.begin();                  //Starting the Wireless communication
-radio.openWritingPipe(address); //Setting the address where we will send the data
-radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
-radio.stopListening();          //This sets the module as transmitter
-
+  radio.begin();                  //Starting the Wireless communication
+  radio.openWritingPipe(address); //Setting the address where we will send the data
+  radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.stopListening();          //This sets the module as transmitter
 }
-void loop()
-{
-if (radio.available()){
-  Serial.println("Success");}
-else{
-  Serial.println("Sad"); 
- }
-input_pos = joystick.angle();
 
-
+void loop(){
   
-const char text[] = "Suck a fuck";
-radio.write(&text, sizeof(text));               
-//radio.write(input_pos);                  //Sending the message to receiver
- 
-delay(1000);
+  input_pos = joystick.angle();
+  Serial.println(input_pos);
+  
+    
+  const char text[] = "Suck a fuck";
+  radio.write(&text, sizeof(text));               
+  //radio.write(input_pos);                  //Sending the message to receiver
+  Serial.println(text); 
+  delay(1000);
 }
