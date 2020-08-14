36924e08e93c346e33682894aec4a6da6d5e6b3d
Kiefer <kiefervermeulen@hotmail.com>
2020-08-14 15:59:52 +0200

    Changed address

--- RC_sender/RC_sender.ino
+++ RC_sender/RC_sender.ino
@@ -4,7 +4,7 @@
 #include <Joystick.h>
 
 RF24 radio(9, 10); // CE, CSN     
-const byte address[6] = "00001";     //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
+const uint64_t pipes[2] = {0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL}; //Byte of array representing the address. This is the address where we will send the data. This should be same on the receiving side.
 
 int pinX = A0;
 int pinY = A1;
@@ -18,9 +18,12 @@ Joystick joystick(pinX, pinY, pinButton);
 void setup() {
   Serial.begin(9600);
   radio.begin();                  //Starting the Wireless communication
-  radio.openWritingPipe(address); //Setting the address where we will send the data
-  radio.setPALevel(RF24_PA_MIN);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
   radio.stopListening();          //This sets the module as transmitter
+ // radio.openWritingPipe(address); //Setting the address where we will send the data
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);
+  radio.setPALevel(RF24_PA_LOW);  //You can set it as minimum or maximum depending on the distance between the transmitter and receiver.
+ 
 }
 
 void loop(){
@@ -29,9 +32,9 @@ void loop(){
   Serial.println(input_pos);
   
     
-  const char text[] = "Suck a fuck";
+  const char text[32] = "S";
   radio.write(&text, sizeof(text));               
   //radio.write(input_pos);                  //Sending the message to receiver
   Serial.println(text); 
-  delay(1000);
+  delay(100);
 }
